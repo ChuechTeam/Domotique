b1f86f469ea8798007b617e4ebb1a830e34eb45a
DynamicField <dynamicfield@outlook.fr>
2025-03-19 12:23:42 +0100

    Again, docs.

--- src/main/java/fr/domotique/site/HomeSection.java
+++ src/main/java/fr/domotique/site/HomeSection.java
@@ -12,10 +12,14 @@ public class HomeSection extends Section {
 
     @Override
     public void register(Router router) {
+        // Link all routes to our functions
         router.get("/").respond(this::index);
     }
 
+    // GET /
+    // ------ The home page of our website
     Future<Buffer> index(RoutingContext ctx) {
+        // Return the "hello.jte" with a value test = 123
         return view(ctx, "hello.jte",
                 viewArg("test", 123));
     }
